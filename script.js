24dc37aec4f9bbf708d7b7d97a7a752ad97902db
I <helagro@outlook.com>
2023-02-28 08:30:17 +0100

    unspecified

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const unscramble = s => atob(s.slice(0, Math.floor(s.length / 2)).split("").reverse().join("") + s.slice(Math.floor(s.length / 2)))
 
 document.getElementById("32649").textContent = unscramble("XdvB0bydWYsVGaRsb29rLmNvbQ==")
+document.getElementById("43168").textContent = unscramble("5kTLzcDMOS05OTk5")
 
