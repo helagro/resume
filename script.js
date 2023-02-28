407d20cf9c731688d1d41767ca4b39773e6adb3a
I <helagro@outlook.com>
2023-02-28 08:00:39 +0100

    unspecified

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-const _32649 = atob("aGVsYWdyb0BvdXRsb29rLmNvbQ==")
+const __32649 = "XdvB0bydWYsVGa".split("").reverse().join("")
+const _32649 = atob(__32649 + "Rsb29rLmNvbQ==")
 document.getElementById("32649").textContent = _32649
