6b73dc7723fcebd0cf9ee902ff08ee35e421a6ea
I <helagro@outlook.com>
2023-02-28 11:00:24 +0100

    unspecified

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+
+// =========== LOAD EMAIL AND PHONE NUMBER ============
+
 const unscramble = s => atob(s.slice(0, Math.floor(s.length / 2.5)).split("").reverse().join("") + s.slice(Math.floor(s.length / 2.5)))
 
 const emailAddr = unscramble("B0bydWYsVGavdXRsb29rLmNvbQ==")
@@ -9,3 +13,16 @@ const phoneNr = unscramble("TLzcDMk5OS05OTk5")
 const phoneElem = document.getElementById("phone")
 phoneElem.textContent = phoneNr
 phoneElem.href = "tel:" + phoneNr
+
+
+
+
+
+function setShowImages(value){
+    const images = document.getElementsByTagName("img")
+    const displayValue = value ? "unset" : "none"
+
+    for(const image of images){
+        image.style.display = displayValue
+    }
+}
