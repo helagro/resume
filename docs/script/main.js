ebc443afc62548a28f0ac170fe0d073202303128
I <helagro@outlook.com>
2023-03-03 19:20:35 +0100

    minor fixes

--- docs/script/main.js
+++ docs/script/main.js
@@ -1,6 +1,3 @@
-
-const showPFP = (value) => document.getElementById("profilePic").style.display = value ? "unset" : "none"
-
 function loadContactDetails(){
     const unscramble = s => atob(s.slice(0, Math.floor(s.length / 2.5)).split("").reverse().join("") + s.slice(Math.floor(s.length / 2.5)))
     const emailAddr = unscramble("B0bydWYsVGavdXRsb29rLmNvbQ==")
@@ -15,13 +12,7 @@ function loadContactDetails(){
     phoneElem.href = "tel:" + phoneNr
 }
 
-function changeLanguage(){
-    const language = document.getElementById("languageSelect").value
-    console.log(language)
-    clearNewRenderedElems()
-    hasFilledContent = false
-    loadContentFile(language)
-}
+const showPFP = (value) => document.getElementById("profilePic").style.display = value ? "unset" : "none"
 
 
 // ============= RUN =============
