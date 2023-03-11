7a6b8e5eeb20c92192cc77e6210ce776857b28a5
Henrik L <helagro@outlook.com>
2023-03-11 20:30:29 +0100

    unspecified

--- docs/script/main.js
+++ docs/script/main.js
@@ -15,10 +15,28 @@ function loadContactDetails(){
 const showPFP = (value) => document.getElementById("profilePic").style.display = value ? "unset" : "none"
 
 
-const optionsContent = document.getElementById("optionsContent")
-function showOptions(){
-    console.log(optionsContent.style)
-    optionsContent.style.display = optionsContent.style.display == "none" ? "unset" : "none"
+let showOptionsPopup = false
+function toggleOptionsPopup(){
+    rotateToggleOptionsPopupBtn()
+    showOptionsPopup = !showOptionsPopup
+
+    const optionsContent = document.getElementById("optionsContent")
+    optionsContent.style.display = showOptionsPopup ? "grid" : "none"
+}
+
+function rotateToggleOptionsPopupBtn(){
+    const img = document.getElementById("toggleOptionsBtnImg")
+    let rotation = 0
+
+    const rotate = () => {
+        img.style.transform = "rotate(" + rotation + "deg)"
+        rotation += 20
+
+        if(rotation <= 180)
+            requestAnimationFrame(rotate)
+    }
+
+    rotate()
 }
 
 
