e47afd2875ecfd2135f4e6b7e897b0f7b3b09ac2
helagro <helagro@outlook.com>
2024-03-17 15:20:57 +0000

    Better hosting

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "resume",
-      script: "serve /home/h/resume/docs -p 8080",
+      script: "node index",
       env: {
         NODE_ENV: "production",
       },
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const rootPath = process.env.ROOT_PATH || '/resume'
+const folderPath = path.join(__dirname, 'docs')
+const port = process.env.PORT || 8080
+
+app.use((req, res, next) => {
+    console.log(`Request received: ${req.method} ${req.url}`)
+    next()
+})
+
+app.use(rootPath, express.static(path.join(__dirname, 'docs')))
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}, hosting ${folderPath}`)
+})
