7e1e27de4f6803c5aea2f607ca7a4ac80337554e
Henrik L <helagro@outlook.com>
2024-03-17 16:38:05 +0100

    unspecified

--- compile.js
+++ compile.js
@@ -1,30 +1,28 @@
 const Handlebars = require("handlebars")
 const fs = require("fs")
 
-
 const languages = [
-    {
-        inputName: "english.json",
-        outputName: "index.html"
-    },
-    {
-        inputName: "swedish.json",
-        outputName: "se.html"
-    },
+  {
+    inputName: "english.json",
+    outputName: "en.html",
+  },
+  {
+    inputName: "swedish.json",
+    outputName: "se.html",
+  },
 ]
 
-
-for(const language of languages){
-    compileLanguage(language)
+for (const language of languages) {
+  compileLanguage(language)
 }
 
-function compileLanguage(lang){
-    const templateSource = fs.readFileSync("source/template.html", "utf8")
-    const contentStr = fs.readFileSync(`source/${lang.inputName}`, "utf8")
+function compileLanguage(lang) {
+  const templateSource = fs.readFileSync("source/template.html", "utf8")
+  const contentStr = fs.readFileSync(`source/${lang.inputName}`, "utf8")
 
-    const contentObj = JSON.parse(contentStr)
-    const template = Handlebars.compile(templateSource)
-    const result = template(contentObj)
-    
-    fs.writeFileSync(`docs/${lang.outputName}`, result)
+  const contentObj = JSON.parse(contentStr)
+  const template = Handlebars.compile(templateSource)
+  const result = template(contentObj)
+
+  fs.writeFileSync(`docs/${lang.outputName}`, result)
 }
